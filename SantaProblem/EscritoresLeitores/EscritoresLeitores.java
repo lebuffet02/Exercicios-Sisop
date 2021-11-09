d5f60d6f9bac195da91b6a4ecdbdc9911fecbf3d
lebuffet02 <lebuffet02@gmail.com>
2021-11-08 22:23:25 -0300

    EPC2 letra A pronta

---
+++ SantaProblem/EscritoresLeitores/EscritoresLeitores.java
@@ -0,0 +1,68 @@
+package EscritoresLeitores;
+
+import java.util.concurrent.Semaphore;
+
+class EscritoresLeitores {
+
+    static Semaphore readLock = new Semaphore(1, true);
+    static Semaphore writeLock = new Semaphore(1, true);
+    volatile static int readCount = 0;
+
+    static class Le implements Runnable {
+        @Override
+        public void run() {
+            try {
+                readLock.acquire();
+                readCount++;
+                if (readCount == 1) {
+                    writeLock.acquire();
+                }
+                readLock.release();
+                System.out.println(Thread.currentThread().getName() + " Lendo. ");
+                Thread.sleep(1500);
+                System.out.println(Thread.currentThread().getName() + " Terminou de Ler. ");
+                readLock.acquire();
+                readCount--;
+                if(readCount == 0) {
+                    writeLock.release();
+                }
+                readLock.release();
+            } 
+            catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    static class Escreve implements Runnable {
+        @Override
+        public void run() {
+            try {
+                writeLock.acquire();
+                System.out.println(Thread.currentThread().getName() + " Está Escrevendo");
+                Thread.sleep(2500);
+                System.out.println(Thread.currentThread().getName() + " has finished WRITING");
+                writeLock.release();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Le read = new Le();
+        Escreve write = new Escreve();
+        Thread t1 = new Thread(read);
+        t1.setName("1 ");
+        Thread t2 = new Thread(read);
+        t2.setName("2 ");
+        Thread t3 = new Thread(write);
+        t3.setName("3 ");
+        Thread t4 = new Thread(read);
+        t4.setName("4 ");
+        t1.start();
+        t3.start();
+        t2.start();
+        t4.start();
+    }
+}
